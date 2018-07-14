754df5f1f5018e554cac03efa41478810f793b01
Paul Uncangco <pju93@hotmail.com>
2018-07-13 22:53:26 -0500

    sublime

--- assets/sublime.js
+++ assets/sublime.js
@@ -1,11 +1,9 @@
-let project_tab = document.getElementsByClassName('titles');
-for(var i = 0; i < project_tab[0].childNodes.length; i++) {
-  if(project_tab[0].childNodes[i].localName == "p") { 
-     console.log(project_tab[0].childNodes[i].innerHTML);
-  }
-}
-console.log(project_tab);
-
+let proj_name = document.getElementsByClassName('tab_space')[0].childNodes[3].innerHTML,
+project_desc =  document.getElementsByClassName('tab_space paragraph')[0].childNodes[1].innerHTML,
+proj_tech =     document.getElementsByClassName('tab_space console')[0].childNodes[5].innerHTML,
+proj_link =     document.getElementsByTagName('a')[0].href,
+project_tab =   document.getElementsByClassName('titles')[0], 
+default_id =    0;
 function requestURL(url, callback) {
   let request = new XMLHttpRequest(), 
   method = 'GET', 
@@ -19,11 +17,32 @@ function requestURL(url, callback) {
   request.send();
 };
 
-function project() {
-  let url = 'assets/project.json';
-  requestURL(url, function(){
-    let data = JSON.parse(this.responseText);
-    console.log(data);
-  });
-}
-project();
+let url = 'assets/project.json';
+requestURL(url, function(){
+  let data = JSON.parse(this.responseText);
+  
+  console.log(data);
+  
+  // Default 
+  proj_name =     data[0].name;
+  project_desc =  data[0].description;
+  proj_tech =     data[0].technolgoies;
+  proj_link =     data[0].link;
+
+  for(var i = 0; i < data.length; i++) {
+    console.log(data[i]);
+    project_tab.children[i].addEventListener('click', function(event){
+      console.log(event);
+    });
+  } 
+
+  // for(var i = 0; i < project_tab.children.length; i++) {
+  //   project_tab.children[i].addEventListener('click', function(event){
+  //     console.log(data[i]);
+  //     // proj_name =     data[i].name;
+  //     // project_desc =  data[i].description;
+  //     // proj_tech =     data[i].technolgoies;
+  //     // proj_link =     data[i].link;
+  //   });
+  // }
+});
